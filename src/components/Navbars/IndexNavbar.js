cab14b1d75e6b6a24c8f0736495ec48d7a287a57
ravieway <ravibs.mail@gmail.com>
2019-12-17 17:15:10 +0530

    edit_lines2

--- src/components/Navbars/IndexNavbar.js
+++ src/components/Navbars/IndexNavbar.js
@@ -51,7 +51,7 @@ function IndexNavbar() {
             data-placement="bottom"
             href="/index"
             target="_blank"
-            title="Coded by Creative Tim"
+            title="cbaselabs"
           >
             Active Carbon
           </NavbarBrand>
